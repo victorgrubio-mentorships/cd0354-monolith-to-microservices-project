684063f51d693f208cbc3bad0e5d4e019b7c2a49
Andrej Kovacic <andrejkovacic123@gmail.com>
2024-01-06 11:02:31 +0100

    Move env variables setup to config file

--- udagram-api-user/src/config/config.ts
+++ udagram-api-user/src/config/config.ts
@@ -1,4 +1,6 @@
-require('dotenv').config()
+import { config as dotenvConfig } from 'dotenv';
+
+dotenvConfig();
 
 export const config = {
   'username': process.env.POSTGRES_USERNAME,
@@ -9,6 +11,8 @@ export const config = {
   'aws_region': process.env.AWS_REGION,
   'aws_profile': process.env.AWS_PROFILE,
   'aws_media_bucket': process.env.AWS_BUCKET,
+  'aws_access_key_id': process.env.AWS_ACCESS_KEY_ID,
+  'aws_secret_access_key': process.env.AWS_SECRET_ACCESS_KEY,
   'url': process.env.URL,
   'jwt': {
     'secret': process.env.JWT_SECRET,
